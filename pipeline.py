94cc3e3c67557e5127aef5fd07b8bb4a9360b069
AjinkyaBhave <ajinkyaybhave@gmail.com>
2017-08-02 12:37:12 +0530

    Adding lane pixel detection.

--- pipeline.py
+++ pipeline.py
@@ -4,29 +4,31 @@ from moviepy.editor import VideoFileClip
 from process_image import *
 
 # File and directory paths
-params_file = 'camera_params.p'
-video_file  = ''
-img_dir     = './test_images/'
-img_file    = 'straight_lines1.jpg'
-#clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,5)
-#clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4")
-#white_clip = clip1.fl_image(pipeline) #NOTE: this function expects color images!!
-#white_clip.write_videofile(white_output, audio=False)
+params_file  = 'camera_params.p'
+video_file   = 'project_video.mp4'
+video_output = 'project_video_output.mp4'
+img_dir      = 'test_images/'
+img_file     = 'test3.jpg'
+
+# Pipeline to process camera image to isolate lane markings
+def pipeline(img):
+    visualise = True
+    img_undist = undistort_image(img, mtx=mtx, dist=dist, visualise=visualise)
+    img_thresh = threshold_image(img_undist, visualise=visualise)
+    img_top = view_road_top(img, img_thresh, visualise=visualise)
+    return img_top
 
 # Read camera intrinsic matrix and distortion coefficients
 # Created initially with calibrate_camera.py
 with open(params_file, mode='rb') as f:
     params = pickle.load(f)
-mtx = params['intrinsic']
+mtx  = params['intrinsic']
 dist = params['distortion']
 
-# Pipeline to process camera image to isolate lane markings
-def pipeline(img):
-    img_undist = undistort_image(img, mtx=mtx, dist=dist, visualise=True)
-    img_thresh = threshold_image(img_undist, visualise=True)
-    img_top = view_road_top(img, img_thresh, visualise=True)
-    return img_top
-
+'''clip1 = VideoFileClip(video_file).subclip(0,5)
+white_clip = clip1.fl_image(pipeline) #NOTE: this function expects color images!!
+white_clip.write_videofile(video_output, audio=False)
+'''
 img = mpimg.imread(img_dir + img_file)
 pipeline(img)
 
